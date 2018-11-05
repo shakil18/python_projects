9dcdae652892483edf65e1dff81a263b28ff43b5
shakil18 <shakil.wave@gmail.com>
2018-11-05 17:36:06 +0100

    Testing code

---
+++ Test.py
@@ -0,0 +1,11 @@
+# Print 1st letters from string variable
+
+import random
+
+str = 'This is a very nice weather. My name is shakil. Nice to meet you!'
+
+print(len (str))
+
+var = random.randint(1,65)
+
+print(str[:10])
